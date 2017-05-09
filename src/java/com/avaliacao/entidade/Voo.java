d5e9618c035b0df12b21bfd9ee10c157c0d7b28c
Aluno <Aluno@dv547388lab05>
2017-05-08 22:16:10 -0300

    troca motorista mateus para jeferson

--- src/java/com/avaliacao/entidade/Voo.java
+++ src/java/com/avaliacao/entidade/Voo.java
@@ -1,15 +1,70 @@
 package com.avaliacao.entidade;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Voo implements Serializable {
     
     @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;    
+    
+    @ManyToMany (fetch = FetchType.EAGER)
+    private List<Aviao> aviao;
+    
+    public Voo(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Aviao> getAviao() {
+        return aviao;
+    }
+
+    public void setAviao(List<Aviao> aviao) {
+        this.aviao = aviao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Voo other = (Voo) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Voo{" + "id=" + id + ", aviao=" + aviao + '}';
+    }
+    
     
 }

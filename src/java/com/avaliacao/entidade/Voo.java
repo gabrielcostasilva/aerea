d7b996efd857641955c471f3e0149c435fc05dde
Aluno <Aluno@dv547388lab05>
2017-05-08 22:38:29 -0300

    troca motorista jeferson para mateus

--- src/java/com/avaliacao/entidade/Voo.java
+++ src/java/com/avaliacao/entidade/Voo.java
@@ -1,6 +1,7 @@
 package com.avaliacao.entidade;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.Entity;
@@ -9,6 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Voo implements Serializable {
@@ -16,8 +20,30 @@ public class Voo implements Serializable {
     @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;    
     
+    @Temporal (TemporalType.DATE)
+    private Date dataVoo;
+    
     @ManyToMany (fetch = FetchType.EAGER)
     private List<Aviao> aviao;
+    
+    @ManyToOne (fetch = FetchType.EAGER)
+    private DestinoVoo destinoVoo;
+
+    public Date getDataVoo() {
+        return dataVoo;
+    }
+
+    public void setDataVoo(Date dataVoo) {
+        this.dataVoo = dataVoo;
+    }
+
+    public DestinoVoo getDestinoVoo() {
+        return destinoVoo;
+    }
+
+    public void setDestinoVoo(DestinoVoo destinoVoo) {
+        this.destinoVoo = destinoVoo;
+    }
     
     public Voo(){
         super();

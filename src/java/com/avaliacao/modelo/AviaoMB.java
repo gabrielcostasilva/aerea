d8e4d0ed4efcc0fa0ea45f7be77ceb0a8e2bfd31
Aluno <Aluno@dv547365lab05>
2017-05-08 21:33:25 -0300

    Incio do projeto.
    Troca de Motorista [Jackson -> Gabriel]

---
+++ src/java/com/avaliacao/modelo/AviaoMB.java
@@ -0,0 +1,15 @@
+package com.avaliacao.modelo;
+
+import javax.ejb.Stateless;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean
+@ViewScoped
+public class AviaoMB {
+    
+    
+
+    
+}
